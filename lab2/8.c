a47427cad80dfafb657b6c0e13edd3eec755cb1e
Piotr Dziegielewski <dexiefy@gmail.com>
2015-11-07 12:33:48 +0100

    add ex 7 and 8

---
+++ lab2/8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+int a, b, c;
+float suma, iloczyn, nw, nm;
+
+  printf("Podaj pierwsza liczbe: ");
+  scanf("%d", &a);
+
+  printf("Podaj druga liczbe: ");
+  scanf("%d", &b);
+
+  printf("Podaj trzecia liczbe: ");
+  scanf("%d", &c);
+
+printf
+
+}
