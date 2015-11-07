11ef2f7701acd2f180df4711df8a12d089806601
Piotr Dziegielewski <dexiefy@gmail.com>
2015-11-07 12:12:39 +0100

    add zad6

---
+++ lab2/6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+  float a, s;
+      printf("Podaj liczbe do spierwiastkowania: ");
+      scanf("%f", &a);
+      s = sqrt(a);
+  if (a<0) {
+      printf("Sprobuj jeszcze raz podajac liczbe dodatnia\n");
+}
+  else {
+      printf("Pierwiastek kwadratowy podanej liczby to= %.10f\n", s);
+}
+}
