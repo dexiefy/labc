1f2deb92677f6e2e514d976fc7432b6d29e92f98
Piotr Dziegielewski <dexiefy@gmail.com>
2015-11-14 12:26:51 +0100

    porzadki

---
+++ lab2/05-petle/4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int n, a, suma=0;
+      printf("Podaj liczbe: ");
+      scanf("%d", &n);
+
+if (n<1) {
+  printf("Podaj liczbe wieksza od 0!\n");
+  exit (1);
+}
+else;
+  for (a=1; a<=n; ++a){
+    suma+=a;
+  }
+printf("Suma liczb = %d\n", suma);
+}
