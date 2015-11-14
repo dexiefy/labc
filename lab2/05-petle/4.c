045802c75eda870458b8410939767dfd01a5d730
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2015-11-14 12:42:16 +0100

    Update 4.c

--- lab2/05-petle/4.c
+++ lab2/05-petle/4.c
@@ -3,17 +3,17 @@
 
 int main() {
 
-  int n, a, suma=0;
+  int n, a, suma = 0;
       printf("Podaj liczbe: ");
       scanf("%d", &n);
 
-if (n<1) {
+if ( n < 1) {
   printf("Podaj liczbe wieksza od 0!\n");
   exit (1);
 }
 else;
-  for (a=1; a<=n; ++a){
-    suma+=a;
+  for ( a = 1 ; a <= n; ++a){
+    suma += a;
   }
 printf("Suma liczb = %d\n", suma);
 }
