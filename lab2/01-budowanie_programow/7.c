2f9f9ac68d2ff1a09d36793f03e9f82826c6ba95
Piotr Dziegielewski <dexiefy@gmail.com>
2015-11-27 10:26:50 +0100

    update 7.c (final state)

--- lab2/01-budowanie_programow/7.c
+++ lab2/01-budowanie_programow/7.c
@@ -9,17 +9,16 @@ int main() {
       printf("Podaj liczbe: ");
       scanf("%f", &a);
       s = sqrt(a);
-      o = 1/a;
-
-   if (a=0) {
+      
+   if (a == 0) {
 	printf("podaj liczbe dodatnia!\n");
 	exit(1);
 }
-   else if (a<0) {
-        printf("Liczba odwrotna do liczby %.0f to: -%.0f\n", a, o);
+   else if (0 > a) {
+      printf("Liczba odwrotna do liczby %.0f to: -1/%.0f\n", a,- a);
 }
    else {
-      printf("blablablabla odwrotna do liczby %.0f to: %.0f\n", a, o);
-      printf("Pierwiastek kwadratowy podanej liczby to: %.5f\n", s);
+      printf("Liczba odwrotna do liczby %.0f to 1/%.0f. A jej pierwiastek kwardratowy wynosi %.5f\n", a, a, s);
+      
 }
 }
