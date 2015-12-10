5344c54f3a4813e80d8ed3eb9374069b1b72e95d
Piotr Dziegielewski <dexiefy@gmail.com>
2015-12-10 20:13:58 +0100

    update 5.c from petle

--- lab2/05-petle/5.c
+++ lab2/05-petle/5.c
@@ -5,12 +5,18 @@
 #include <stdlib.h>
 
 int main(){
-      int tablica[6];
+  int n;
+  int t[n];
+  size_t i;
 
-	printf ("Podaj 6 liczb calkowitych: ");
-	scanf ("%d" "%d" "%d" "%d" "%d" "%d", &tablica[1], &tablica[2], &tablica[3], &tablica[4], &tablica[5], &tablica[6]);
-	printf ("%d | %d\n%d | %d\n%d | %d\n", tablica[1], tablica[2], tablica[3], tablica[4], tablica[5], tablica[6]);
+printf ("Podaj liczbe elementow: \n");
+    scanf("%d\n", &n);
 
-printf("\n%d %d %d %d %d %d\n", tablica[1], tablica[2], tablica[3], tablica[4], tablica[5], tablica[6]);
-printf ("\n%d %d %d %d %d %d\n", tablica[6], tablica[5], tablica[4], tablica[3], tablica [2], tablica[1]);
+printf("Podaj %d liczb calkowitych: \n", n);
+ for (i = 1; i <=n; i++) {
+   scanf ("%d", &t[n]);
+   printf("%d ", t[n]);
+}
+
+
 }
