5bef3e7310fd44c846a0dd0309fc6d1ba661a632
Piotr Dziegielewski <dexiefy@gmail.com>
2015-12-10 20:10:39 +0100

    add 08 1.c

--- lab2/08-Elementarz2/1.c
+++ lab2/08-Elementarz2/1.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int main() {
-  int M;
-  int suma = 0;
-  int i = 1;
-  int n;
+  int M, suma = 0, i = 1;
+{
+  printf ( "Podaj liczbe M:" );
+  scanf ( "%d", &M );
+}
 
-  printf("Podaj liczbe M: ");
-  scanf("%d\n", &M);
+  while ( suma < M ) {
+     suma = suma+i;
+     i = i+1;
+}
 
+  printf("Szukane n to:%d\n", i);
 
-  while (suma < M); {
-     suma=suma+i;
-     i=i+1;
-  }
-  n=i+1;
-
-  printf("Szukane n to: %d", n);
- 
 }
