cd8b23c9f95d23d6969327af6c30cfe405001e5f
Piotr Dziegielewski <dexiefy@gmail.com>
2015-12-10 20:15:12 +0100

    nothing

---
+++ lab2/07-funkcje/4.c
@@ -0,0 +1,13 @@
+// 4. Napisz funkcję, która po wczytaniu liczby całkowitej wypisze jej cyfry
+// zaczynając od ostatniej i kończąc na pierwszej.
+// Na przykład po wczytaniu liczby '1410' funkcja powinna wypisać '0141'.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (){
+
+
+
+}
