36cf5a08710038f7c395cbe83a987b4ef130c41e
Piotr Dziegielewski <dexiefy@gmail.com>
2015-12-12 12:22:59 +0100

    add 1c from Elementarz3

---
+++ lab2/10-Elementarz3/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+
+int c;
+
+while ((c=getchar())!=EOF){
+putchar(c);
+}
+
+if (c=='\t'){
+printf("\\t");
+}
+else if (c=='\\'){
+printf("\\\\");
+}
+else{
+return 0;
+}
+}
