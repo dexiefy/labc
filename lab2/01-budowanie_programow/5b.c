71ef0e103008a4867fb974cffdc38116940cde6b
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2015-12-17 14:11:42 +0100

    Update 5b.c

--- lab2/01-budowanie_programow/5b.c
+++ lab2/01-budowanie_programow/5b.c
@@ -2,28 +2,21 @@
    wypisuje, która z nich jest większa. */
 #include <stdio.h>
 #include <stdlib.h>
+
 int main() {
 
-   int a, b, result_a, result_b;
-   char *msg = "wieksza liczba to: %d\n";
+int a, b;
+char *msg = "Wieksza liczba to: %d\n";
 
+   printf("Podaj dwie liczby: ");
+   scanf("%d%d", &a, &b);
 
-      printf("Podaj pierwsza liczbe: ");
-      result_a = scanf("%d", &a);
-      if (result_a==0) {
-        printf("Error: blad w konwersji pierwszej liczby\n");
-        exit(1);
-      }
-
-      printf("Podaj druga liczbe: ");
-      result_b = scanf("%d", &b);
-      if (result_b==0) {
-        printf("Error: blad w konwersji pierwszej liczby\n");
-        exit(1);
-      }
-if (a > b)
-  printf(msg, a);
-else {
-  printf(msg, b);
+     if (a > b){
+       printf(msg, a);}
+     else if (b > a){
+       printf(msg, b);}
+     else {
+       printf("Liczby sa rowne!\n");
 }
 }
+
