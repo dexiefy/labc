0892ab4aba3c6832cbcc5edb720d74950acde29f
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2015-12-17 16:57:37 +0100

    Update 1.c

--- lab2/10-Elementarz3/1.c
+++ lab2/10-Elementarz3/1.c
@@ -1,3 +1,27 @@
+/*1. W poniższym programie, który kopiując wejście na wyjście zastępuje kazdy znak 
+tabulacji przez \t oraz każdy znak \ przez dwa takie znaki, zostały przestawione wiersze. 
+Uporzadkuj je, tak aby powstał działający program.
+
+#include <stdio.h>
+while ((c=getchar())!=EOF)
+}
+printf("\\t");
+else if (c=='\\')
+printf("\\\\");
+if (c=='\t')
+else
+int c;
+putchar(c);
+int main() {
+return 0;
+*/
+
+
+
+
+
+
+
 #include <stdio.h>
 
 int main() {
