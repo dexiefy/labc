1a250804d22027d8a8e267c7d4532a93f3ccecfe
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2015-12-17 16:54:32 +0100

    Update 1.c

--- lab2/08-Elementarz2/1.c
+++ lab2/08-Elementarz2/1.c
@@ -1,19 +1,25 @@
-//1. W poniższym programie, który kopiując wejście na wyjście zastępuje kazdy znak tabulacji przez \t
-//oraz każdy znak \ przez dwa takie znaki, zostały przestawione wiersze.
-//Uporzadkuj je, tak aby powstał działający program.
-//
-//#include <stdio.h>
-//while ((c=getchar())!=EOF)
-//}
-//printf("\\t");
-//else if (c=='\\')
-//printf("\\\\");
-//if (c=='\t')
-//else
-//int c;
-//putchar(c);
-//int main() {
-//return 0;
+/*1. Poniższy – zawierający wiele błędów – program dla podanej z klawiatury liczby M ma obliczyć najmniejszą liczbę n, taką że
+
+1 + 2 + 3 +...+ n >= M
+
+Popraw wszystkie błędy w tym programie tak, aby poprawiony program prawidłowo obliczał liczbę n.
+
+#include <stdio.h>
+int main() {
+  int M;
+  suma = 0;
+  i = 1;
+  print("Podaj liczbe M: ");
+  scanf("%f\n", %M)
+  while (suma < M) {
+     suma=suma+i;
+     i=i+1;
+  }
+  n=i+1;
+  printf("Szukane n to: ", n);
+  return 0;
+}
+*/
 
 
 
