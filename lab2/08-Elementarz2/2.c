c287911892be44578dc538d64a274a168fbdcfbf
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2015-12-17 16:56:52 +0100

    Update 2.c

--- lab2/08-Elementarz2/2.c
+++ lab2/08-Elementarz2/2.c
@@ -1,4 +1,17 @@
-//2. Napisz program, wypisujący w postaci tabelki pierwiastki z pierwszych 20 liczb naturalnych.
+//2. Zamiast komentarzy /* ... */ w programie poniżej wpisz kod tak, aby powstał program, 
+//który sprawdza czy podana liczba całkowita jest pierwsza.
+
+//#include <stdio.h>
+//int main() {
+/* deklaracje zmiennych */
+//  printf("Podaj liczbe calkowita: ");
+  /* wczytaj tę liczbę do zmiennej n */
+  /* sprawdź czy n jest liczbą pierwszą */
+//printf("Podana liczba '%d' jest %s\n",
+//    n, jest_pierwsza ? "pierwsza" : "złożona");
+//  return 0;
+//}
+
 
 
 #include <stdio.h>
