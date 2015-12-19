ebcbe6c69e815a6effed52d42271d7ad1cdeebbe
Piotr Dziegielewski <dexiefy@gmail.com>
2015-12-19 11:52:23 +0100

    add 1.c Elementarz1

---
+++ lab2/04-Elementarz1/1.c
@@ -0,0 +1,20 @@
+/*1. Napisz program obliczający wartość każdego z poniższych wyrażeń:
+
+    n1=5+3*8/2-3
+    n2=2%2+2*2-2/2
+    n3=2*4*(5+9/2)
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int n1 = 5+3*8/2-3;
+  int n2 = 2%2+2*2-2/2;
+  int n3 = 2*4*(5+9/2);
+
+  printf("5+3*8/2-3 = %d\n", n1);
+  printf("2mod2+2*2-2/2 = %d\n", n2);
+  printf("2*4*(5+9/2) = %d\n", n3);
+
+}
