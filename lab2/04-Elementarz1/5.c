083fa735146985e7aff2cab084ea89bffc05c9d1
Piotr Dziegielewski <dexiefy@gmail.com>
2015-12-19 12:16:22 +0100

    add 5.c

---
+++ lab2/04-Elementarz1/5.c
@@ -0,0 +1,37 @@
+/*5. Student G. Apa dostał od kolegi na kartce kod programu zliczającego
+ znaki odstępu, tabulacji i nowego wiersza. Niestety jego młodszy brat
+ pociął kartkę na kawałki zawierające po jednym wierszu kodu,
+ a następnie kawałki te wymieszał. Uporządkuj te kawałki,
+  tak aby powstał działający program.
+
+ zlicz znaki odstępu, tabulacji
+{
+}
+#include &lt;stdio.h>
+i nowego wiersza */
+
+#include <stdio.h>
+
+int main (){
+
+int c;
+int nb, nt, nl;
+
+nb = 0;
+nt = 0;
+nl = 0;
+
+while ((c=getchar()) != EOF){
+if (c == ' ')
+else if (c == '\n')
+else if (c == '\t')
+++nb;
+++nl;
+++nt;
+}
+
+printf("%d %d %d\n", nb, nt, nl);
+
+
+return 0;
+}
