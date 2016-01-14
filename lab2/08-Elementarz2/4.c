b314d432c8505ac050ad209c7e661de10ec4ac78
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2016-01-14 18:41:53 +0100

    Update 4.c

--- lab2/08-Elementarz2/4.c
+++ lab2/08-Elementarz2/4.c
@@ -1,27 +1,35 @@
-//4. Zamiast komentarza /** ... */ i kropek ... poniżej wpisz kod,
-//tak aby powstał program zliczający znaki odstępu, tabulacji i nowego wiersza tekstu wczytanego z stdin.
+//4. Zamiast komentarza /** ... */ i kropek ... poniżej wpisz kod, tak aby powstał
+//program zliczający znaki odstępu, tabulacji i nowego wiersza tekstu wczytanego z stdin.
+//#include <stdio.h>
+//int main() {
+//   /** deklaracje zmiennych lokalnych */
+//   nb = 0;  /* liczba znaków odstępu */
+//   nt = 0;  /* liczba znaków tabulacji */
+//   nl = 0;  /* liczba znaków nowego wiersza */
+//   while ((c=getchar()) != EOF) {
+//      /** zliczaj białe znaki */
+//   }
+//   printf("liczba znaków odstępu = %d,
+//      tabulacji = %d, nowego wiersza = %d\n", ... );
+//   return 0;
+//}
 
 #include <stdio.h>
 
-
-int main() {
-
-  /** deklaracje zmiennych lokalnych */
-  nb = 0;  /* liczba znaków odstępu */
-  nt = 0;  /* liczba znaków tabulacji */
-  nl = 0;  /* liczba znaków nowego wiersza */
+int main () {
+  int c, nb=0, nt=0, nl=0;
 
   while ((c=getchar()) != EOF) {
-
-    if (c == '\n')
-    ++nl;
-    else if (c == '\t')
-    ++nt;
-    else if (c == ' ')
-    ++nb;
+    if (c == ' ') {
+      ++nb;
   }
-
+      
+    else if (c == '\t') {
+      ++nt;
+  }
+      
+    else if (c == '\n') {
+      ++nl;}
+  }
   printf("liczba znaków odstępu = %d, tabulacji = %d, nowego wiersza = %d\n", nb, nt, nl);
-
-  return 0;
 }
