a0f51172449887175c6b498784ec875a73707527
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2016-01-14 19:35:48 +0100

    Update 4i.c

--- lab2/11-Rekurencja/4i.c
+++ lab2/11-Rekurencja/4i.c
@@ -5,21 +5,20 @@
 
 int nwd(int a,int b)
 {
-  int c;                    // Tworzymy zmienną c o typie int
-  while(b != 0)             // Tworzymy pętlę działającą gdy b ≠ 0
+  int c;
+  while(b != 0)
   {
-    c = a % b;              // Zmienna c przyjmuje wartość a modulo b
-    a = b;                // Przypisujemy b do a
-    b = c;                // Przypisujemy c do b
+    c = a % b;
+    a = b;
+    b = c;
   }
-  return a;                 // Zwracamy a, czyli żądane NWD(a,b)
+  return a;
 }
+
 int main(){
   int a, b;
-
-
+  
   printf("podaj 2 liczby\n");
   scanf("%d %d", &a, &b);
-
   printf("nwd(%d,%d) = %d\n", a, b, nwd(a,b));
 }
