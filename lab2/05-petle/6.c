2982717357a5209e12463c4bda28344ff390ec9b
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2016-01-14 20:45:33 +0100

    Update 6.c

--- lab2/05-petle/6.c
+++ lab2/05-petle/6.c
@@ -1,27 +1,30 @@
-/* 6. Napisz program, który wczyta ze stdin 6 liczb rzeczywistych do tablicy,
-a następnie wyszukai wypisze największą i najmniejszą liczbę umieszczoną w tej tablicy. */
+//6. Napisz program, który wczyta ze stdin 6 liczb rzeczywistych do tablicy,
+//a następnie wyszuka i wypisze największą i najmniejszą liczbę umieszczoną w tej tablicy.
 
 #include <stdio.h>
+#include <stdlib.h>
 
+#define size 6
 
-int main()
-{
-
-  float tablica[5];
-
-  printf("Podaj 6 liczb rzeczywistych: \n");
-  scanf("%f %f %f %f %f %f", &tablica[0], &tablica[1], &tablica[2],
-  &tablica[3], &tablica[4], &tablica[5]);
-
+int main(){
+  
+  float tablica[size];
+  
+  printf("Podaj 6 elementow tablicy!\n");
+  for(int i = 0; i < size; i++){
+    scanf("%f", &tablica[i]);
+  }
+  
   float min = tablica[0];
   float max = tablica[0];
-
-  for (int i = 0; i < 6; i++) {
-    float x = tablica[i];
-    if (x > max) {
-      max = x;
-    } else if (x < min) {
-      min = x;
+  
+  for (int a = 0; a < size; a++) {
+    float tab = tablica[a];
+    if (tab > max) {
+      max = tab;
+    }
+    else if (tab < min) {
+      min = tab;
     }
   }
   printf("min = %f\nmax = %f\n", min, max);
