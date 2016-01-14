e3587979a630e87d12ea9787ab28ee5448e1fc7f
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2016-01-14 19:30:24 +0100

    Update perf3a.c

--- lab2/07-funkcje/perf3a.c
+++ lab2/07-funkcje/perf3a.c
@@ -3,22 +3,18 @@
 
 int main() {
   int i, j, sum = 0;
-
+  
   for ( i=1; i <= 1000000 ; i++ ){
     sum = 0;
-
-    /* Checks whether the current number i is Perfect number or not */
-    for(j=1; j<i; j++){
-      if(i%j==0){
-
+    
+    for(j = 1; j < i; j++){
+      if( i%j == 0){
+        
         sum += j;
       }
     }
-    /*
-    * If the current number i is Perfect number
-    */
-    if(sum==i){
-
+    if( sum == i){
+      
       printf("%d is Perfect Number\n", i);
     }
   }
