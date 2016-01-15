b0f7ab20b136cedf944a00ac08e520f254da11f6
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2016-01-15 16:51:22 +0100

    Update 1.c

--- lab2/10-Elementarz3/1.c
+++ lab2/10-Elementarz3/1.c
@@ -1,5 +1,5 @@
-/*1. W poniższym programie, który kopiując wejście na wyjście zastępuje kazdy znak
-tabulacji przez \t oraz każdy znak \ przez dwa takie znaki, zostały przestawione wiersze.
+/*1. W poniższym programie, który kopiując wejście na wyjście zastępuje każdy znak tabulacji 
+przez \t oraz każdy znak \ przez dwa takie znaki, zostały przestawione wiersze. 
 Uporzadkuj je, tak aby powstał działający program.
 
 #include <stdio.h>
@@ -16,22 +16,20 @@ int main() {
 return 0;
 */
 
+
 #include <stdio.h>
-
-int main() {
-
+int main()
+{
   int c;
-
   while ((c=getchar())!=EOF){
-    putchar(c);
+    
+    if (c=='\t'){
+      printf("\\t");
+    }
+    else if (c=='\\'){
+      printf("\\\\");
+    }
   }
-  if (c=='\t'){
-    printf("\\t");
-  }
-  else if (c=='\\'){
-    printf("\\\\");
-  }
-  else{
-    return 0;
-  }
+  putchar(c);
+  return 0;
 }
