544fb2ca164ac1f36ea2d4a016cbb991f6c0b746
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2016-01-15 22:46:39 +0100

    Update fact.c

--- lab2/11-Rekurencja/fact.c
+++ lab2/11-Rekurencja/fact.c
@@ -1,29 +1,19 @@
 #include <stdio.h>
 
-
-
 int  main() {
-     
-int i;
-    
- printf("Podaj liczbe ktorej silnie chcesz obliczyc!\n");
-    
- scanf("%d", &i);
-    
- 
-   printf("Factorial of %d is %d\n", i, factorial(i));
   
- return 0;
+  int i;
+  
+  printf("Podaj liczbe ktorej silnie chcesz obliczyc!\n");
+  scanf("%d", &i);
+  
+  printf("Silnia liczby %d to %d\n", i, factorial(i));
 }
 
-
 int factorial(int i) {
-
- 
+  
   if(i <= 1) {
-   
-   return 1;
-   }
- 
+    return 1;
+  }
   return i * factorial(i - 1);
 }
